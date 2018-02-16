4fb8777a3231fd7f72c7edab2148f074d98fb054
Seo2 <seo2@modosantiago.com>
2018-02-16 14:51:14 -0300

    Desarrollo del home

--- assets/js/livinn.js
+++ assets/js/livinn.js
@@ -1,8 +1,28 @@
 
-$(".owl-carousel").owlCarousel({
+$("#carousel1").owlCarousel({
 	 items:1,
 	 loop: true,
 	 autoplay: true,
-	 nav: true,
+	 nav: false,
 	 dots: false
+});
+
+$("#carousel2").owlCarousel({
+	 items:1,
+	 loop: true,
+	 autoplay: true,
+	 nav: false,
+	 dots: true
+});
+
+$('[data-toggle="tooltip"]').tooltip();
+
+$('#lugares1 a').on('click', function(){
+	$('.lugarboxinfo').addClass('hide');
+	elid = $(this).attr('id');
+	
+	$('#txt-'+elid).removeClass('hide');
+	console.log('../img/'+elid+'.jpg');
+	$('#lugarboxfoto').css({'background-image':'url(assets/img/'+elid+'.jpg)'});
+	
 });
